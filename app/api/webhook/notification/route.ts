8ea48c0f25d94dcc2a31515106c07980eb8a1943
alanapapa <berikbazar@yandex.com>
2025-05-29 12:01:45 +0500

    webhook

--- app/api/webhook/notification/route.ts
+++ app/api/webhook/notification/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import { headers } from 'next/headers';
 import { prisma } from '@/lib/prisma';
 import { ApplicationStatus } from '@prisma/client';
+import { getTranslations } from 'next-intl/server';
 
 export async function POST(request: Request) {
   try {
@@ -48,39 +49,52 @@ export async function POST(request: Request) {
     }
 
     // Определяем новый статус на основе status из body
+    const tTrustMeStatus = await getTranslations('TrustMeStatus');
+    let statusText = '';
     let newStatusId: ApplicationStatus;
     switch (body.status) {
       case 0:
+        statusText = tTrustMeStatus('notSigned');
         newStatusId = 'NEED_SIGNATURE';
         break;
       case 1:
+        statusText = tTrustMeStatus('companySigned');
         newStatusId = 'NEED_SIGNATURE';
         break;
       case 2:
+        statusText = tTrustMeStatus('clientSigned');
         newStatusId = 'NEED_SIGNATURE';
         break;
       case 3:
+        statusText = tTrustMeStatus('fullSigned');
         newStatusId = 'CHECK_DOCS';
         break;
       case 4:
+        statusText = tTrustMeStatus('revokedCompany');
         newStatusId = 'RE_PROCESSING';
         break;
       case 5:
+        statusText = tTrustMeStatus('companyInitiatedTermination');
         newStatusId = 'NEED_SIGNATURE_TERMINATE_CONTRACT';
         break;
       case 6:
+        statusText = tTrustMeStatus('clientInitiatedTermination');
         newStatusId = 'RE_PROCESSING';
         break;
       case 7:
+        statusText = tTrustMeStatus('clientRefusedTermination');
         newStatusId = 'CHECK_DOCS';
         break;
       case 8:
+        statusText = tTrustMeStatus('terminated');
         newStatusId = 'RE_PROCESSING';
         break;
       case 9:
-        newStatusId = 'RE_PROCESSING';
+        statusText = tTrustMeStatus('clientRefusedSignature');
+        newStatusId = 'RE_PROCESSING'; // REFUSED_TO_SIGN
         break;
       default:
+        statusText = tTrustMeStatus('unknownStatus');
         newStatusId = 'NEED_SIGNATURE';
     }
 
@@ -89,7 +103,7 @@ export async function POST(request: Request) {
       data: {
         applicationId: application.id,
         statusId: newStatusId,
-        description: `TrustMe webhook: status ${body.status}`,
+        description: `TrustMe webhook: ${statusText}`,
       },
     });
 
