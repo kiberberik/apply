a5c4111f6bf9a6e295ef5ee5af656fc61f818a52
berik <berikbazar@yandex.com>
2025-07-24 18:26:30 +0500

    PlatonusBtn loading, other templates

--- components/applicationForm/PlatonusButton.tsx
+++ components/applicationForm/PlatonusButton.tsx
@@ -263,7 +263,30 @@ const PlatonusButton = ({ application }: { application: any }) => {
         onClick={() => handleSendToPlatonus()}
         disabled={isLoading}
       >
-        Platonus
+        {isLoading ? (
+          <svg
+            className="h-5 w-5 animate-spin text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+            ></path>
+          </svg>
+        ) : (
+          'Platonus'
+        )}
       </Button>
     </div>
   );
